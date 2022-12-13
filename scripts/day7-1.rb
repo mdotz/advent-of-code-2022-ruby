45f4db7146a9368b71343115ba4884a48b464f87
mdotz <marcin.x.zielinski@gmail.com>
2022-12-13 20:36:51 +0100

    delete useless ||=

--- scripts/day7-1.rb
+++ scripts/day7-1.rb
@@ -11,12 +11,12 @@ File.open(ARGV[0], "r").each do |line|
     pointer = tree.dig(*current_path)
   elsif /\$ cd .*/.match?(line)
     name = /\$ cd (.*)/.match(line).captures.first
-    pointer[name] ||= { type: :dir, size: 0 }
+    pointer[name] = { type: :dir, size: 0 }
     current_path << name
     pointer = pointer[name]
   elsif /^dir/.match?(line)
     name = /^dir (.*)/.match(line).captures.first
-    pointer[name] ||= { type: :dir, size: 0 }
+    pointer[name] = { type: :dir, size: 0 }
   elsif /^[0-9]+/.match?(line)
     size, name = /([0-9]+) (.*)/.match(line).captures
     pointer[name] = { type: :file, size: size.to_i }
