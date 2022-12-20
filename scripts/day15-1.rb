09ce93fa9ba598fc91b8824ab325cc721aea5e5c
mdotz <marcin.x.zielinski@gmail.com>
2022-12-20 13:10:28 +0100

    Day 15, Part 1 - optimize

--- scripts/day15-1.rb
+++ scripts/day15-1.rb
@@ -5,8 +5,10 @@ require "set"
 line_regex = /.*x=(-?[0-9]+), y=(-?[0-9]+):.*x=(-?[0-9]+), y=(-?[0-9]+)/
 goal_row_beacons = Set.new
 goal_y = ARGV[1].to_i
+min_x = Float::INFINITY
+max_x = -Float::INFINITY
 
-sensor_coverage = File.open(ARGV[0], "r").each_with_object([]) do |line, arr|
+sensor_coverage = File.open(ARGV[0], "r").each do |line|
   sx, sy, bx, by = line_regex.match(line).captures.map(&:to_i)
   goal_row_beacons.add([bx, by]) if by == goal_y
   distance = (sx - bx).abs + (sy - by).abs
@@ -16,7 +18,10 @@ sensor_coverage = File.open(ARGV[0], "r").each_with_object([]) do |line, arr|
   max_coverage = distance * 2 + 1
   deduction = (sy - goal_y).abs * 2
   goal_row_coverage = max_coverage - deduction
-  arr << [*(sx - goal_row_coverage / 2)..(sx + goal_row_coverage / 2)]
+  min = sx - goal_row_coverage / 2
+  max = sx + goal_row_coverage / 2
+  min_x = min if min < min_x
+  max_x = max if max > max_x
 end
 
-puts sensor_coverage.reduce(:+).uniq.size - goal_row_beacons.size
+puts max_x - min_x + 1 - goal_row_beacons.size
