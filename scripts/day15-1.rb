43897b685fe4673997dc1029227ecb49edc5f89a
mdotz <marcin.x.zielinski@gmail.com>
2023-01-08 18:32:15 +0100

    Day 15, Part 2

--- scripts/day15-1.rb
+++ scripts/day15-1.rb
@@ -3,25 +3,25 @@
 require "set"
 
 line_regex = /.*x=(-?[0-9]+), y=(-?[0-9]+):.*x=(-?[0-9]+), y=(-?[0-9]+)/
-goal_row_beacons = Set.new
-goal_y = ARGV[1].to_i
+target_row_beacons = Set.new
+target_y = ARGV[1].to_i
 min_x = Float::INFINITY
 max_x = -Float::INFINITY
 
-sensor_coverage = File.open(ARGV[0], "r").each do |line|
+File.open(ARGV[0], "r").each do |line|
   sx, sy, bx, by = line_regex.match(line).captures.map(&:to_i)
-  goal_row_beacons.add([bx, by]) if by == goal_y
+  target_row_beacons.add([bx, by]) if by == target_y
   distance = (sx - bx).abs + (sy - by).abs
 
-  next if (sy - distance).abs >= goal_y
+  next if (sy - distance).abs >= target_y
 
-  max_coverage = distance * 2 + 1
-  deduction = (sy - goal_y).abs * 2
-  goal_row_coverage = max_coverage - deduction
-  min = sx - goal_row_coverage / 2
-  max = sx + goal_row_coverage / 2
+  diagonal = distance * 2 + 1
+  deduction = (sy - target_y).abs * 2
+  target_row_coverage = diagonal - deduction
+  min = sx - target_row_coverage / 2
+  max = sx + target_row_coverage / 2
   min_x = min if min < min_x
   max_x = max if max > max_x
 end
 
-puts max_x - min_x + 1 - goal_row_beacons.size
+puts max_x - min_x + 1 - target_row_beacons.size
---
+++ scripts/day15-2.rb
@@ -0,0 +1,76 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+def segments_intersection(a, b, c, d)
+  abc = ((d.x - c.x) * (a.y - c.y)) - ((d.y - c.y) * (a.x - c.x))
+  abd = ((d.y - c.y) * (b.x - a.x)) - ((d.x - c.x) * (b.y - a.y))
+
+  return nil if abd.zero?
+
+  t = abc.fdiv(abd)
+  ba = Point.new((b.x - a.x) * t, (b.y - a.y) * t)
+
+  Point.new(a.x + ba.x, a.y + ba.y)
+end
+
+def point_inside_square?(square, m)
+  a = square.a
+  b = square.b
+  d = square.d
+  am = Point.new(m.x - a.x, m.y - a.y)
+  ab = Point.new(b.x - a.x, b.y - a.y)
+  ad = Point.new(d.x - a.x, d.y - a.y)
+  abab = (ab.x * ab.x) + (ab.y * ab.y)
+  adad = (ad.x * ad.x) + (ad.y * ad.y)
+  amab = (am.x * ab.x) + (am.y * ab.y)
+  amad = (am.x * ad.x) + (am.y * ad.y)
+
+  (0 < amab && amab < abab) && (0 < amad && amad < adad)
+end
+
+line_regex = /.*x=(-?[0-9]+), y=(-?[0-9]+):.*x=(-?[0-9]+), y=(-?[0-9]+)/
+left_slope_lines = []
+right_slope_lines = []
+squares = []
+Point = Struct.new(:x, :y)
+Segment = Struct.new(:point1, :point2)
+Square = Struct.new(:a, :b, :c, :d)
+
+File.open(ARGV[0], "r").each do |line|
+  sx, sy, bx, by = line_regex.match(line).captures.map(&:to_i)
+  distance = (sx - bx).abs + (sy - by).abs
+
+  top = Point.new(sx, sy + distance)
+  bottom = Point.new(sx, sy - distance)
+  right = Point.new(sx + distance, sy)
+  left = Point.new(sx - distance, sy)
+
+  right_slope_lines += [Segment.new(left, bottom), Segment.new(top, right)]
+  left_slope_lines += [Segment.new(top, left), Segment.new(right, bottom)]
+  squares << Square.new(left, top, right, bottom)
+end
+
+intersections = right_slope_lines.each_with_object([]) do |r_line, arr|
+  left_slope_lines.each do |l_line|
+    intersection_point = segments_intersection(r_line.point1, r_line.point2, l_line.point1, l_line.point2)
+    points = [r_line.point1, r_line.point2, l_line.point1, l_line.point2]
+    if intersection_point && points.all? { _1 != intersection_point }
+      arr << intersection_point
+    end
+  end
+end
+
+intersections.uniq.each_with_object([]) do |point, arr|
+  if intersections.include?(Point.new(point.x + 1, point.y + 1)) &&
+      intersections.include?(Point.new(point.x + 2, point.y)) &&
+      intersections.include?(Point.new(point.x + 1, point.y - 1))
+    potential_target = Point.new(point.x + 1, point.y)
+
+    if squares.none? { point_inside_square?(_1, potential_target) } &&
+        potential_target.x >= 0 && potential_target.x <= 4_000_000 &&
+        potential_target.y >= 0 && potential_target.y <= 4_000_000
+      puts (potential_target.x * 4_000_000 + potential_target.y).to_i
+      return
+    end
+  end
+end
